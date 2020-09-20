952c4926014878b321621d309f0ab1eb1d773e8e
Nikita-Poltavets <mykyta.poltavets@nure.ua>
2020-09-20 14:59:46 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+In order to start the application, enter the command in the terminal:
+"mvn clean install". Next, in the Maven() window, the application itself
+is launched along the next path: "Plugins -> tomcat7 -> tomcat7:run-war".
+Then in your browser go to the corresponding path printed in the terminal.
+ 
---
+++ src/main/java/com/practice/socket/FirstServlet.java
@@ -0,0 +1,44 @@
+package com.practice.socket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "first-servlet", urlPatterns = "/")
+public class FirstServlet extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(FirstServlet.class);
+    Map<String, String> userAgentAndIP = new HashMap<>();
+
+    @Override
+    public void init() {
+        log.info("Sample Servlet initialized");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter responseBody = resp.getWriter();
+
+        resp.setContentType("text/html");
+
+        userAgentAndIP.put(req.getHeader("user-agent"), req.getRemoteAddr());
+
+        for(Map.Entry<String, String> entry : userAgentAndIP.entrySet()){
+            responseBody.println("<h3 align=\"center\">"+ "<b>" + entry.getValue() + " :: " + entry.getKey() + "</b>" + " </h3>");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        log.info("Sample Servlet destroyed");
+    }
+}
